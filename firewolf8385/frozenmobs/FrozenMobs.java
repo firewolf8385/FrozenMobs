02c108681c2b66c4950bde8cc47ecd229961726d
Colby <colbykrawczyk10@gmail.com>
2019-08-15 15:33:35 -0400

    Cleaned up FrozenMobs.java

--- firewolf8385/frozenmobs/FrozenMobs.java
+++ firewolf8385/frozenmobs/FrozenMobs.java
@@ -1,5 +1,6 @@
 package firewolf8385.frozenmobs;
 
+import firewolf8385.frozenmobs.commands.FM;
 import firewolf8385.frozenmobs.events.CreatureSpawn;
 import firewolf8385.frozenmobs.events.EntityExplode;
 import firewolf8385.frozenmobs.events.EntityTarget;
@@ -15,16 +16,34 @@ public class FrozenMobs extends JavaPlugin
      *    Date: August 15th, 2019
      *    Code version: Beta 1.0
      ***************************************************************************************/
+    SettingsManager settings = SettingsManager.getInstance();
 
     @Override
     public void onEnable()
     {
+        // Creates/Loads the config.yml
+        settings.setup(this);
+
         // Enables bStats
         Metrics metrics = new Metrics(this);
 
+        // Registers Events used by the plugin.
+        registerEvents();
+
+        // Registers Commands used by the plugin.
+        registerCommands();
+    }
+
+    private void registerEvents()
+    {
         Bukkit.getServer().getPluginManager().registerEvents(new CreatureSpawn(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new EntityExplode(), this);
         Bukkit.getServer().getPluginManager().registerEvents(new EntityTarget(), this);
+    }
+
+    private void registerCommands()
+    {
+        getCommand("frozenmobs").setExecutor(new FM());
     }
 
 }
