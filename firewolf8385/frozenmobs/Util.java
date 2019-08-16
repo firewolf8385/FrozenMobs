d5e6c8aab70121403672bd5fbd4221500f293bea
Colby <colbykrawczyk10@gmail.com>
2019-08-16 01:26:54 -0400

    Added A Utitily
    This is to get the type of entity as a string. Just makes things a little cleaner later on.

---
+++ firewolf8385/frozenmobs/Util.java
@@ -0,0 +1,13 @@
+package firewolf8385.frozenmobs;
+
+import org.bukkit.entity.*;
+
+public class Util
+{
+
+    public static String getEntityType(Entity e)
+    {
+        return e.getType().toString();
+    }
+
+}
