ee7ff335d4a8f7f8eeec5e571a54cb654b02c0b7
Colby <colbykrawczyk10@gmail.com>
2019-08-18 14:24:53 -0400

    Fixed Bug Caused By Fixing Phantoms

--- firewolf8385/frozenmobs/events/CreatureSpawn.java
+++ firewolf8385/frozenmobs/events/CreatureSpawn.java
@@ -5,31 +5,33 @@ import firewolf8385.frozenmobs.Util;
 import org.apache.logging.log4j.core.net.Priority;
 import org.bukkit.entity.Phantom;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import sun.plugin.com.Utils;
 
 public class CreatureSpawn implements Listener
 {
     SettingsManager settings = SettingsManager.getInstance();
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onSpawn(CreatureSpawnEvent e)
     {
         if(settings.getConfig().getBoolean(Util.getEntityType(e.getEntity())))
         {
-            if(e.getEntity() instanceof Phantom)
+            PotionEffect effect = new PotionEffect(PotionEffectType.SLOW, 99999, 99999);
+            e.getEntity().addPotionEffect(effect);
+
+            if(!settings.getConfig().getBoolean("Collidable"))
             {
-                e.setCancelled(true);
+                e.getEntity().setCollidable(false);
+            }
 
-                PotionEffect effect = new PotionEffect(PotionEffectType.SLOW, 99999, 99999);
-                e.getEntity().addPotionEffect(effect);
-
-                if(!settings.getConfig().getBoolean("Collidable"))
-                {
-                   e.getEntity().setCollidable(false);
-                }
+            if(Util.getEntityType(e.getEntity()).equals("PHANTOM"))
+            {
+                e.getEntity().setAI(false);
             }
         }
     }
